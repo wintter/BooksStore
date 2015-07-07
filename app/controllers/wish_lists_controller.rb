baadfe42a7f585b1ff65c58004a860b7aa7a4889
wintter <chesterfield252@gmail.com>
2015-07-07 13:33:49 +0300

    use rescue_from

--- app/controllers/wish_lists_controller.rb
+++ app/controllers/wish_lists_controller.rb
@@ -1,5 +1,5 @@
 class WishListsController < ApplicationController
-  #before_action :check_current_user, only: [:show]
+  rescue_from ActiveRecord::RecordNotUnique, with: :record_not_unique
 
   def show
     @user = User.find(params[:id])
@@ -11,19 +11,21 @@ class WishListsController < ApplicationController
   end
 
   def update
-    begin
       cur_user.books.push Book.find(params[:id])
       flash[:success] = 'Book has added to wish list'
-    rescue ActiveRecord::RecordNotUnique
-      flash[:success] = 'You already add this book to wish list'
-    ensure
       redirect_to (:back)
-    end
   end
 
   def destroy
     cur_user.books.delete(Book.find params[:id])
     redirect_to action: 'show', id: cur_user.id
   end
+
+  private
+
+    def record_not_unique
+      flash[:success] = 'You already add this book to wish list'
+      redirect_to (:back)
+    end
 
 end
