e72eacd9667ac6a908906a8c2e1e8c7d7f8a636b
wintter <chesterfield252@gmail.com>
2015-07-09 16:37:43 +0300

    add methods

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,4 +2,18 @@ class Rating < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
   scope :approved, -> { where('review IS NOT NULL') }
+
+  class << self
+
+    def get_rate(book, user)
+      @rating = Rating.where(book: book, user: user).first
+      @rating ? @rating = @rating.rating_number : @rating = 0
+    end
+
+    def get_review(book)
+      @review = Rating.where(book: book, approve: true).all
+    end
+
+  end
+
 end
