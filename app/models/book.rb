8c6dcb9eedad0d536f6b162b833bd570d98eb0bf
wintter <chesterfield252@gmail.com>
2015-07-09 16:37:34 +0300

    add search method

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,20 @@ class Book < ActiveRecord::Base
   has_many :ratings
   has_and_belongs_to_many :users
   validates :title, :description, :price, :in_stock, presence: true
+
+  scope :by_text, ->(text) { where("title LIKE '%#{text}%'") }
+
+  class << self
+
+    def search type, text
+      if type.eql? '1'
+        Book.by_text text
+      else
+        @author = Author.by_author text
+        Book.where(author: @author)
+      end
+    end
+
+  end
+
 end
