a566352027454a8a93a7622737c743a5aa5119cf
wintter <chesterfield252@gmail.com>
2015-07-09 16:37:24 +0300

    add scope

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,6 @@
 class Author < ActiveRecord::Base
   validates :firstname, :lastname, :biography, presence: true
   has_many :books
+
+  scope :by_author, ->(author) { where("firstname LIKE '%#{author}%' OR lastname LIKE '%#{author}%'") }
 end
