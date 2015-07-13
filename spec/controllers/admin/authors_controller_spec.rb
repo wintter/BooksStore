bb807d919fb67e1b8c5b327d2d9556ff31227e32
wintter <chesterfield252@gmail.com>
2015-07-13 12:57:56 +0300

    push spec for authors

--- spec/controllers/admin/authors_controller_spec.rb
+++ spec/controllers/admin/authors_controller_spec.rb
@@ -2,131 +2,131 @@ require 'rails_helper'
 
 RSpec.describe Admin::AuthorsController, type: :controller do
 
-  before do
-	  allow_message_expectations_on_nil
-    allow(controller).to receive(:check_login_user).and_return true
-    allow(controller).to receive(:check_admin).and_return true
-  end
+	before do
+		allow_message_expectations_on_nil
+		allow(controller).to receive(:check_login_user).and_return true
+		allow(controller).to receive(:check_admin).and_return true
+	end
 
-  shared_examples "#create, #update with valid arguments" do
-	  describe 'redirect' do
+	shared_examples "#create, #update with valid arguments" do
+		describe 'redirect' do
 
-		  before { request }
+			before { request }
 
-		  it 'to :index' do
-			  expect(response).to redirect_to(:admin_authors)
-		  end
+			it 'to :index' do
+				expect(response).to redirect_to(:admin_authors)
+			end
 
-		  it 'and success flash' do
-			  expect(flash[:success]).to be_present
-		  end
+			it 'and success flash' do
+				expect(flash[:success]).to be_present
+			end
 
-	  end
-  end
+		end
+	end
 
-  shared_examples "#create, #update with invalid arguments" do
-	  describe 'render' do
+	shared_examples "#create, #update with invalid arguments" do
+		describe 'render' do
 
-		  before { request }
+			before { request }
 
-		  it 'templates' do
-			  expect(response).to render_template(templates)
-		  end
+			it 'templates' do
+				expect(response).to render_template(templates)
+			end
 
-	  end
-  end
+		end
+	end
 
-  describe 'GET #index' do
+	describe 'GET #index' do
 
-    it 'use #all' do
-      expect(Author).to receive(:all)
-      get :index
-    end
+		it 'use #all' do
+			expect(Author).to receive(:all)
+			get :index
+		end
 
-  end
+	end
 
-  describe 'GET #new' do
+	describe 'GET #new' do
 
-    it 'use #new' do
-      expect(Author).to receive(:new)
-      get :new
-    end
+		it 'use #new' do
+			expect(Author).to receive(:new)
+			get :new
+		end
 
-  end
+	end
 
-  describe 'GET #edit' do
+	describe 'GET #edit' do
 
-    let(:author) { FactoryGirl.create(:author) }
+		let(:author) { FactoryGirl.create(:author) }
 
-    before do
-      allow(Author).to receive(:find).and_return author
-    end
+		before do
+			allow(Author).to receive(:find).and_return author
+		end
 
-    it 'use #find' do
-      expect(Author).to receive(:find)
-      get :edit, id: 1
-    end
+		it 'use #find' do
+			expect(Author).to receive(:find)
+			get :edit, id: 1
+		end
 
-    it 'correct find' do
-      get :edit, id: 1
-      expect(assigns(:author)).to eq author
-    end
+		it 'correct find' do
+			get :edit, id: 1
+			expect(assigns(:author)).to eq author
+		end
 
-  end
+	end
 
-  describe 'POST #create' do
+	describe 'POST #create' do
 
-	  context 'with valid attributes' do
-		  let(:author_params) { FactoryGirl.attributes_for(:author) }
+		context 'with valid attributes' do
+			let(:author_params) { FactoryGirl.attributes_for(:author) }
 
-		   before do
-			  allow(assigns(:author)).to receive(:save).and_return true
-			  allow(assigns(:author)).to receive(:firstname).and_return ''
-		  end
+			before do
+				allow(assigns(:author)).to receive(:save).and_return true
+				allow(assigns(:author)).to receive(:firstname).and_return ''
+			end
 
-		  it 'receives new with params' do
-			  expect(Author).to receive(:new).with(author_params)
-			  post :create, author: author_params
-		  end
+			it 'receives new with params' do
+				expect(Author).to receive(:new).with(author_params)
+				post :create, author: author_params
+			end
 
-		  it_behaves_like "#create, #update with valid arguments" do
-			  let(:request) { post :create, author: author_params }
-		  end
+			it_behaves_like "#create, #update with valid arguments" do
+				let(:request) { post :create, author: author_params }
+			end
 
-		  it_behaves_like "#create, #update with invalid arguments" do
-			  let(:request) { post :create, author: {firstname: 'Bob'} }
-			  let(:templates) { :new }
-		  end
+			it_behaves_like "#create, #update with invalid arguments" do
+				let(:request) { post :create, author: {firstname: 'Bob'} }
+				let(:templates) { :new }
+			end
 
-	  end
+		end
 
-  end
+	end
 
-  describe 'PATCH #update' do
-	  let(:author_params) { FactoryGirl.attributes_for(:author) }
-	  let(:author) { FactoryGirl.create(:author) }
+	describe 'PATCH #update' do
+		let(:author_params) { FactoryGirl.attributes_for(:author) }
+		let(:author) { FactoryGirl.create(:author) }
 
-	  before do
-		  @params = {:id => '1', :author => author_params}
-		  allow(Author).to receive(:find).and_return author
-		  post :update, @params
-	  end
+		before do
+			@params = {:id => '1', :author => author_params}
+			allow(Author).to receive(:find).and_return author
+			post :update, @params
+		end
 
-	  it 'call #find' do
+		it 'call #find' do
 			expect(Author).to receive(:find).with("1")
-		  patch :update, @params
-	  end
+			patch :update, @params
+		end
 
-	  it_behaves_like "#create, #update with valid arguments" do
-		  let(:request) { patch :update, @params }
-	  end
+		it_behaves_like "#create, #update with valid arguments" do
+			let(:request) { patch :update, @params }
+		end
 
-	  it_behaves_like "#create, #update with invalid arguments" do
-		  let(:request) { patch :update, id: 3, author: {firstname: ''} }
-		  let(:templates) { :edit }
-	  end
+		it_behaves_like "#create, #update with invalid arguments" do
+			let(:request) { patch :update, id: 3, author: {firstname: ''} }
+			let(:templates) { :edit }
+		end
 
-  end
+	end
 
 	describe 'DELETE #destroy' do
 		let(:author) { FactoryGirl.create(:author) }
