287ce530d4210cba0d87493dbcc0b7f953caeb77
wintter <chesterfield252@gmail.com>
2015-07-14 10:09:02 +0300

    add key and secret code

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,11 @@
+if Rails.env == 'development'
+  code = '412182575633605'
+  secret_key = '05a765cea352c7bd2df2bdcb22cb8f73'
+else
+  code = '1619406021650927'
+  secret_key = '68ca7c28f6d8b62e59bf994615308f4f'
+end
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '412182575633605', '05a765cea352c7bd2df2bdcb22cb8f73',
+  provider :facebook, code, secret_key,
            :scope => 'email', :display => 'popup'
 end
