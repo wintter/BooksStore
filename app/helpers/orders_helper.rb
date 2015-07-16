177ae791bd8795f1fdd40ad3ae2182cdefa941ef
wintter <chesterfield252@gmail.com>
2015-07-16 11:54:15 +0300

    add methods

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,19 @@
 module OrdersHelper
+
+  def get_session_value(entity, type)
+    if session[entity]
+      session[entity][type]
+    else
+      nil
+    end
+  end
+
+  def get_checked_value(entity, type)
+    true if type.eql?('5') || entity.eql?(type)
+  end
+
+  def remove_key(hash)
+    hash.except!('id', 'created_at', 'updated_at')
+  end
+
 end
