37250d901af81f66535fe9d3a2b8e719f396434d
wintter <chesterfield252@gmail.com>
2015-07-16 11:53:57 +0300

    remove some methods

--- app/helpers/auths_helper.rb
+++ app/helpers/auths_helper.rb
@@ -11,7 +11,7 @@ module AuthsHelper
   end
 
   #return user by token
-  def cur_user
+  def current_user
     User.find_by(remember_token: AuthsHelper.encrypt(cookies[:remember_token]))
   end
 
@@ -20,19 +20,9 @@ module AuthsHelper
     !User.find_by(remember_token: AuthsHelper.encrypt(cookies[:remember_token])).nil?
   end
 
-  #callback check admin
-  def check_admin
-    redirect_to root_url unless cur_user.admin?
-  end
-
-  #callback for check current user, user with id 50 cant /users/51/edit
-  def check_current_user
-    redirect_to root_path, notice: 'Permission denied' unless User.find(params[:id]).eql? cur_user
-  end
-
   #callback for watch login user on site
   def check_login_user
-    redirect_to new_auth_path, notice: 'Sign in for watching.' unless cur_user
+    redirect_to new_auth_path, notice: 'Sign in for watching.' unless current_user
   end
 
   class << self
