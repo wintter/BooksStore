e35d59721d68b6012e5a132c3312eeb54db378ec
wintter <chesterfield252@gmail.com>
2015-07-30 15:51:26 +0300

    remove dropbox upload

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -7,14 +7,14 @@ class CoverUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :dropbox
-  #storage :file
+  #storage :dropbox
+  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    #"uploads/#{model.class.to_s.underscore}/#{mounted_as}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
