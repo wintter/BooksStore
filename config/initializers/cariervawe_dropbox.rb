ca7f14a2dd53a174e78f4d47f918bc5e55504e28
wintter <chesterfield252@gmail.com>
2015-07-30 12:41:00 +0300

    add cariervawe dropbox

---
+++ config/initializers/cariervawe_dropbox.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+  config.dropbox_app_key = 'zzyneja4f7gypsl'
+  config.dropbox_app_secret = '7s86p5zpl4dcjbe'
+  config.dropbox_access_token = 'zszqcxc75608zjkp'
+  config.dropbox_access_token_secret = '8pi2lb0c8m3cto4'
+  config.dropbox_user_id = '332224931'
+  config.dropbox_access_type = "app_folder"
+end
