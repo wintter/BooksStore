6d9dc7535b4f3487cc67c9990426aff178c97f48
wintter <chesterfield252@gmail.com>
2015-08-06 14:37:39 +0300

    add address checker

--- app/controllers/concerns/order_checker.rb
+++ app/controllers/concerns/order_checker.rb
@@ -1,26 +1,45 @@
 module OrderChecker
 
   def check_previous_step
-    return redirect_to wizard_path(:order_address) unless @cart.address
+    return redirect_to wizard_path(:order_address) unless @cart.billing_address && @cart.shipping_address
     return redirect_to wizard_path(:order_delivery) unless @cart.delivery
     return redirect_to wizard_path(:order_payment) unless @cart.credit_card
     render_wizard
   end
 
-  def check_valid_request(entity, params)
-    @info = entity.new(params)
-    if @info.valid?
-      @cart.create_address(params) if entity.eql? Address
-      @cart.create_credit_card(params) if entity.eql? CreditCard
+  def create_credit_card(params)
+    @credit_card = CreditCard.new(params)
+    if @credit_card.valid?
+      unless @cart.credit_card
+        @cart.create_credit_card(params)
+      else
+        @cart.credit_card.update(params)
+      end
     else
-      @address = @info if entity.eql? Address
-      @credit_card = @info if entity.eql? CreditCard
       return false
     end
     true
   end
 
-  def change_delivery(params)
+  def create_addresses(billing, shipping)
+    @billing_address = Address.new(billing)
+    @shipping_address = Address.new(shipping)
+
+    if @billing_address.valid? && @shipping_address.valid?
+      if @cart.billing_address && @cart.shipping_address
+        @cart.billing_address.update(billing)
+        @cart.shipping_address.update(shipping)
+      else
+        @cart.update(billing_address: @billing_address, shipping_address: @shipping_address)
+      end
+      true
+    else
+      false
+    end
+
+  end
+
+  def create_delivery(params)
     if params
       @cart.update(delivery_id: params)
       true
