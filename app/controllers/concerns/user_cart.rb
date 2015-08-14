723aae7deb4e8689cc8059052b6c3b5329480b68
wintter <chesterfield252@gmail.com>
2015-08-14 14:29:56 +0300

    refactoring

--- app/controllers/concerns/user_cart.rb
+++ app/controllers/concerns/user_cart.rb
@@ -6,7 +6,7 @@ module UserCart
 
   def calculate_price
     return @cart.update(total_price: nil) unless @cart.order_items
-    price = Book.where(id: @cart.order_items.pluck(:book_id)).sum(:price)
+    price = @cart.order_items.map { |item| item.quantity*item.book.price }.inject(&:+)
     @cart.coupon ? (discount = @cart.coupon.discount.to_d) : (discount = 0)
     @cart.update(total_price: price - discount)
   end
